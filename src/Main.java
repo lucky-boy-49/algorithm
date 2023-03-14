4be0f6b58ac0aa29b34627e1069ae48eb18dbe32
HJ <1744709356@qq.com>
2023-03-14 14:43:26 +0800

    704.二分查找

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        // 控制台接收字符
+        String str = sc.nextLine();
+        HashMap<Character, Integer> map = new HashMap<>();
+        // 转小写
+        str = str.toLowerCase();
+        for (int i = 0;i<str.length();i++){
+            char c = str.charAt(i);
+            // 这里判断就只会有字符a-z进去，其他的进不去 else哪里也不用在判断
+            if (c >= 'a' && c <= 'z') {
+                if(map.containsKey(c)){
+                    // 如果有+1
+                    map.put(c,map.get(c)+1);
+                }else {
+                    // 没有为1
+                    map.put(c,1);
+                }
+            }
+        }
+        Integer maxNum = Collections.max(map.values());
+        Set<Character> set = new HashSet<>();
+        for (Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue().equals(maxNum)){
+                set.add(entry.getKey());
+            }
+        }
+        String s = "";
+        for (Character i: set) {
+            s += i;
+        }
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        for(int i = 0;i<c.length;i++){
+            System.out.println(c[i]);
+        }
     }
 }
---
+++ src/simple/BinarySearch.java
@@ -0,0 +1,51 @@
+package simple;
+
+/**
+ * @author HJ
+ * @version 1.0.0
+ * @ClassName BinarySearch.java
+ * @Description 二分查找算法
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * 示例 1:
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * 示例 2:
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ * @createTime 2023年03月14日 14:11:00
+ */
+public class BinarySearch {
+
+    /**
+     * @title search
+     * @description 
+     * @author HJ 
+     * @updateTime 2023/3/14 下午2:14 
+     * @throws 
+     */
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length-1;
+        while (left <= right) {
+            int mid = (left+right)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5};
+        int target = 5;
+        BinarySearch binarySearch = new BinarySearch();
+        int search = binarySearch.search(nums, target);
+        System.out.println(search);
+    }
+
+}
