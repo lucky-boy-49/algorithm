633e8f6631fc70424b316510d6a9193cc30c0a31
HJ <1744709356@qq.com>
2023-03-15 19:43:50 +0800

    977. 有序数组的平方

---
+++ src/simple/SquareOfSortedArray.java
@@ -0,0 +1,25 @@
+package simple;
+
+import java.util.Arrays;
+
+/**
+ * @author HJ
+ * 977. 有序数组的平方
+ */
+public class SquareOfSortedArray {
+
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = (int) Math.pow(nums[i], 2);
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    public static void main(String[] args){
+        SquareOfSortedArray squareOfSortedArray = new SquareOfSortedArray();
+        int[] nums = {-4, -1, 0, 3, 10};
+        Arrays.stream(squareOfSortedArray.sortedSquares(nums)).forEach(System.out::println);
+    }
+
+}
