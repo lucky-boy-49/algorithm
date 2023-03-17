f9f5324267da82d00278d6058a3b4afe392fe211
HJ <1744709356@qq.com>
2023-03-17 14:09:42 +0800

    344. 反转字符串

---
+++ src/simple/ReverseString.java
@@ -0,0 +1,26 @@
+package simple;
+/**
+ * @author HJ
+ * 344. 反转字符串
+ */
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        char temp;
+        for(int i = 0; i < s.length / 2; i++) {
+          temp = s[i];
+          s[i] = s[s.length - i - 1];
+          s[s.length - i - 1] = temp;
+        }
+    }
+
+    public static void main(String[] args){
+        ReverseString reverseString = new ReverseString();
+        char[] s = {'h', 'e', 'l', 'l', 'e'};
+        reverseString.reverseString(s);
+        for(int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+
+}
