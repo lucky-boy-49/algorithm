c7c1d4a21f1ecd96c577f18da13945f6e3108bbd
HJ <1744709356@qq.com>
2023-03-19 23:10:55 +0800

    876. 链表的中间结点

---
+++ src/simple/MiddleNodeOfLinkedList.java
@@ -0,0 +1,29 @@
+package simple;
+/**
+ * 876. 链表的中间结点
+ * @author HJ
+ */
+public class MiddleNodeOfLinkedList {
+
+	public ListNode2 middleNode(ListNode2 head) {
+		ListNode2 q = head, p = head;
+		while (q != null && q.next != null) {
+			q = q.next.next;
+			p = p.next;
+		}
+		return p;
+	}
+
+	public static void main(String[] args){
+
+	}
+
+}
+
+class ListNode2 {
+   int val;
+   ListNode2 next;
+   ListNode2() {}
+   ListNode2(int val) { this.val = val; }
+   ListNode2(int val, ListNode2 next) { this.val = val; this.next = next; }
+}
