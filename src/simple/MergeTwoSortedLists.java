16be1c1a18a608798a3bf5dd84cae2ec43d3d590
HJ <1744709356@qq.com>
2023-03-20 22:20:11 +0800

    21. 合并两个有序链表

---
+++ src/simple/MergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+package simple;
+
+public class MergeTwoSortedLists {
+
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		if (list1 == null && list2 == null) {
+            return list1;
+        }
+        ListNode result, end;
+		if (list1 == null && list2 != null) {
+			return list2;
+		} else if (list1 != null && list2 == null) {
+			return list1;
+		}
+        if (list1 != null && list2 != null && list1.val < list2.val) {
+            result = end = list1;
+            result.next = null;
+            list1 = list1.next;
+        } else {
+            result = end = list2;
+            result.next = null;
+            list2 = list2.next;
+        }
+		while (list1 != null && list2 != null) {
+			if (list1.val < list2.val) {
+				end.next = list1;
+				end = list1;
+				list1 = list1.next;
+				end.next = null;
+			} else {
+				end.next = list2;
+				end = list2;
+				list2 = list2.next;
+				end.next = null;
+			}
+		}
+		if (list1 == null) {
+			end.next = list2;
+		} else {
+			end.next = list1;
+		}
+		return result;
+	}
+
+}
