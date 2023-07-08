21e12db08b75dec85034bba1f062315060fa1ff1
贺佳 <hejia1744709356@163.com>
2023-07-08 22:30:37 +0800

    628. 三个数的最大乘积

---
+++ src/test/simple/MaxProductTest.java
@@ -0,0 +1,15 @@
+package test.simple;
+
+import main.simple.MaxProduct;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxProductTest {
+
+    @Test
+    void maximumProduct() {
+        MaxProduct maxProduct = new MaxProduct();
+        System.out.println(maxProduct.maximumProduct(new int[]{1, 2, 3, 4}));
+    }
+}
