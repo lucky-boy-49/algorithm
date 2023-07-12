70a4beeaccbe0c99ca1be05c28dc6b64de034b4a
贺佳 <hejia1744709356@163.com>
2023-07-12 13:45:40 +0800

    BinaryTreeInorderTraversal

---
+++ src/main/medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,30 @@
+package main.medium;
+
+import main.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * BinaryTreeInorderTraversal 是一个对二叉树执行中序遍历并返回按排序顺序的值列表的类。
+ * 使用深度优先搜索（DFS）方法递归地遍历该树。
+ */
+public class BinaryTreeInorderTraversal {
+
+    private final List<Integer> result = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return result;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        result.add(root.value);
+        dfs(root.right);
+    }
+
+}
