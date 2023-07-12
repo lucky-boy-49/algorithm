ef0cba62597d72d8dc5370dc230bc39cb3cd20f5
贺佳 <hejia1744709356@163.com>
2023-07-12 14:27:50 +0800

    Binary Tree Postorder Traversal

---
+++ src/main/simple/BinaryTreePostorderTraversal.java
@@ -0,0 +1,39 @@
+package main.simple;
+
+import main.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Binary Tree Postorder Traversal
+ */
+public class BinaryTreePostorderTraversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.left == null && root.right == prev) {
+                result.add(root.value);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return result;
+    }
+
+}
