796b548ac969a1541be832aac84ac36195a0e194
贺佳 <hejia1744709356@163.com>
2023-07-12 11:46:31 +0800

    394. 字符串解码

---
+++ src/main/medium/DecodeString.java
@@ -0,0 +1,71 @@
+package main.medium;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ * 394. 字符串解码
+ */
+public class DecodeString {
+    int ptr;
+
+    /**
+     * Decodes a given string using a specific algorithm.
+     *
+     * @param s the string to be decoded
+     * @return the decoded string
+     */
+    public String decodeString(String s) {
+        LinkedList<String> stk = new LinkedList<String>();
+        ptr = 0;
+
+        while (ptr < s.length()) {
+            char cur = s.charAt(ptr);
+            if (Character.isDigit(cur)) {
+                // 获取一个数字并进栈
+                String digits = getDigits(s);
+                stk.addLast(digits);
+            } else if (Character.isLetter(cur) || cur == '[') {
+                // 获取一个字母并进栈
+                stk.addLast(String.valueOf(s.charAt(ptr++)));
+            } else {
+                ++ptr;
+                LinkedList<String> sub = new LinkedList<String>();
+                while (!"[".equals(stk.peekLast())) {
+                    sub.addLast(stk.removeLast());
+                }
+                Collections.reverse(sub);
+                // 左括号出栈
+                stk.removeLast();
+                // 此时栈顶为当前 sub 对应的字符串应该出现的次数
+                int repTime = Integer.parseInt(stk.removeLast());
+                StringBuilder t = new StringBuilder();
+                String o = getString(sub);
+                // 构造字符串
+                while (repTime-- > 0) {
+                    t.append(o);
+                }
+                // 将构造好的字符串入栈
+                stk.addLast(t.toString());
+            }
+        }
+
+        return getString(stk);
+    }
+
+    public String getDigits(String s) {
+        StringBuilder ret = new StringBuilder();
+        while (Character.isDigit(s.charAt(ptr))) {
+            ret.append(s.charAt(ptr++));
+        }
+        return ret.toString();
+    }
+
+    public String getString(LinkedList<String> v) {
+        StringBuilder ret = new StringBuilder();
+        for (String s : v) {
+            ret.append(s);
+        }
+        return ret.toString();
+    }
+}
