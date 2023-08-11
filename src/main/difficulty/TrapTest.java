cb757e8aed2aea61987030a48c851856fdc27c4e
贺佳 <hejia1744709356@163.com>
2023-08-11 10:23:58 +0800

    42. 接雨水

---
+++ src/main/difficulty/TrapTest.java
@@ -0,0 +1,13 @@
+package main.difficulty;
+
+import org.junit.jupiter.api.Test;
+
+class TrapTest {
+
+    @Test
+    void solution() {
+
+        System.out.println(new Trap().solution(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+
+    }
+}
