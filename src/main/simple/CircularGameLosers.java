ff5517965b1dd96b5a53d5aa32d6dc2a34e31961
贺佳 <hejia1744709356@163.com>
2023-08-16 22:37:29 +0800

    2682. 找出转圈游戏输家

---
+++ src/main/simple/CircularGameLosers.java
@@ -0,0 +1,27 @@
+package main.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CircularGameLosers {
+
+    public int[] circularGameLosers(int n, int k) {
+        boolean[] visit = new boolean[n];
+        for (int i = k, j = 0; !visit[j]; i += k) {
+            visit[j] = true;
+            j = (j + i) % n;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                list.add(i + 1);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+}
---
+++ src/test/simple/CircularGameLosersTest.java
@@ -0,0 +1,15 @@
+package test.simple;
+
+import main.simple.CircularGameLosers;
+import org.junit.jupiter.api.Test;
+
+class CircularGameLosersTest {
+
+    @Test
+    void circularGameLosers() {
+        int[] ints = new CircularGameLosers().circularGameLosers(1, 1);
+        for (int anInt : ints) {
+            System.out.print(anInt + " ");
+        }
+    }
+}
