6505a4a623336398cce3b9c49b7a06107d508e56
贺佳 <hejia1744709356@163.com>
2023-08-17 13:40:29 +0800

    6. N 字形变换

---
+++ src/test/medium/ConvertTest.java
@@ -0,0 +1,12 @@
+package test.medium;
+
+import main.medium.Convert;
+import org.junit.jupiter.api.Test;
+
+class ConvertTest {
+
+    @Test
+    void convert() {
+        System.out.println(new Convert().convert("PAYPALISHIRING", 3));
+    }
+}
