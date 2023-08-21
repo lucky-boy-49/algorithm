82716f3169120f4838bf43ddb88cabe8684e6a03
贺佳 <hejia1744709356@163.com>
2023-08-21 10:30:57 +0800

    122. 买卖股票的最佳时机 II

---
+++ src/test/medium/BestTimeBuySellStockIITest.java
@@ -0,0 +1,13 @@
+package test.medium;
+
+import main.medium.BestTimeBuySellStockII;
+import org.junit.jupiter.api.Test;
+
+class BestTimeBuySellStockIITest {
+
+    @Test
+    void maxProfit() {
+        BestTimeBuySellStockII b = new BestTimeBuySellStockII();
+        System.out.println(b.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+}
