119915280e3f9a853f9962f6936ca9224c6a14e7
贺佳 <hejia1744709356@163.com>
2023-08-23 10:40:57 +0800

    42. 接雨水 第二次

--- src/main/difficulty/Trap.java
+++ src/main/difficulty/Trap.java
@@ -13,15 +13,15 @@ public class Trap {
         int[] leftMax = new int[len];
         leftMax[0] = height[0];
         for (int i = 1; i < len; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
         int[] rightMax = new int[len];
         rightMax[len - 1] = height[len - 1];
-        for (int i = len - 2; i >= 0; --i) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        for (int i = len - 2; i > -1; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
         int ans = 0;
-        for (int i = 0; i < len; ++i) {
+        for (int i = 1; i < len; i++) {
             ans += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
         return ans;
