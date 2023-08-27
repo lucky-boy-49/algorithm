4e9031de0430e5c0024f3ba70565b6c6cd0f990f
贺佳 <hejia1744709356@163.com>
2023-08-27 22:14:10 +0800

    49. 字母异位词分组

---
+++ src/main/medium/GroupAnagrams.java
@@ -0,0 +1,23 @@
+package main.medium;
+
+import java.util.*;
+
+/**
+ * 49. 字母异位词分组
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
