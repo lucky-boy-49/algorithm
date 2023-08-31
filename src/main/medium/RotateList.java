0138ac795bf133f6c5a5f6b435f1797ad81ad459
贺佳 <hejia1744709356@163.com>
2023-08-31 11:35:22 +0800

    61. 旋转链表

---
+++ src/main/medium/RotateList.java
@@ -0,0 +1,31 @@
+package main.medium;
+
+/**
+ * 61. 旋转链表
+ */
+public class RotateList {
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null) {
+            return head;
+        }
+        int n = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            iter = iter.next;
+            n++;
+        }
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+        iter.next = head;
+        while (add-- > 0) {
+            iter = iter.next;
+        }
+        ListNode ret = iter.next;
+        iter.next = null;
+        return ret;
+    }
+
+}
