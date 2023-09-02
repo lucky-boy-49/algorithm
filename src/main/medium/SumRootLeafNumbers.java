6ba6a398cc188d5f043600fd614cbe4db1a6dd65
HJ <1744709356@qq.com>
2023-09-02 09:53:52 +0800

    129. 求根节点到叶节点数字之和

--- src/main/medium/SumRootLeafNumbers.java
+++ src/main/medium/SumRootLeafNumbers.java
@@ -1,5 +1,8 @@
 package main.medium;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author HJ
  * @date 2023/9/2 09:40
@@ -8,7 +11,33 @@ package main.medium;
 public class SumRootLeafNumbers {
 
     public int sumNumbers(TreeNode root) {
-
+        int res = 0;
+        Deque<TreeNode> queueNode = new LinkedList<>();
+        Deque<Integer> numNode = new LinkedList<>();
+        queueNode.offer(root);
+        numNode.offer(0);
+        while (!queueNode.isEmpty() && !numNode.isEmpty()) {
+            TreeNode node = queueNode.poll();
+            Integer num = numNode.poll();
+            if (node.left == null && node.right == null) {
+                if (num != null) {
+                    num = num * 10 + node.val;
+                    res += num;
+                }
+            } else {
+                if (num != null) {
+                    if (node.left != null) {
+                        queueNode.offer(node.left);
+                        numNode.offer(num * 10 + node.val);
+                    }
+                    if (node.right != null) {
+                        queueNode.offer(node.right);
+                        numNode.offer(num * 10 + node.val);
+                    }
+                }
+            }
+        }
+        return res;
     }
 
 }
