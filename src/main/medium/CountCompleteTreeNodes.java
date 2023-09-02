7d17d2ac03cc8f12e1ec4c9bcd5d6fc60d77f40c
HJ <1744709356@qq.com>
2023-09-02 11:15:33 +0800

    222. 完全二叉树的节点个数

---
+++ src/main/medium/CountCompleteTreeNodes.java
@@ -0,0 +1,46 @@
+package main.medium;
+
+/**
+ * @author HJ
+ * @date 2023/9/2 10:41
+ * @description 222. 完全二叉树的节点个数
+ */
+public class CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+
+}
