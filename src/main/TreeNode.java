c39ea04726d728575e9d7040d0088583e500088d
HJ <1744709356@qq.com>
2023-09-02 10:10:29 +0800

    124. 二叉树中的最大路径和

--- src/main/TreeNode.java
+++ src/main/TreeNode.java
@@ -2,7 +2,7 @@ package main;
 
 public class TreeNode {
 
-    public int value;
+    public int val;
     public TreeNode left;
 
     public TreeNode right;
@@ -10,12 +10,12 @@ public class TreeNode {
     public TreeNode() {
     }
 
-    public TreeNode(int value) {
-        this.value = value;
+    public TreeNode(int val) {
+        this.val = val;
     }
 
-    public TreeNode(int value, TreeNode left, TreeNode right) {
-        this.value = value;
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
         this.left = left;
         this.right = right;
     }
