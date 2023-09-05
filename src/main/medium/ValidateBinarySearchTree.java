831be179e14fde553aa3d101a90483fc283da072
HJ <1744709356@qq.com>
2023-09-05 19:05:01 +0800

    98. 验证二叉搜索树

---
+++ src/main/medium/ValidateBinarySearchTree.java
@@ -0,0 +1,24 @@
+package main.medium;
+
+/**
+ * @author HJ
+ * @date 2023/9/5 18:34
+ * @description
+ */
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+}
