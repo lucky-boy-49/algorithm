aaee38a572effe29a8685f231d40be1c996afd4d
HJ <1744709356@qq.com>
2023-09-12 09:47:44 +0800

    1462. 课程表 IV

---
+++ src/main/medium/CourseScheduleIV.java
@@ -0,0 +1,52 @@
+package main.medium;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author HJ
+ * @date 2023/9/12 09:14
+ * @description 1462. 课程表 IV
+ */
+public class CourseScheduleIV {
+
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<Integer>[] g = new List[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            g[i] = new ArrayList<>();
+        }
+        int[] indgree = new int[numCourses];
+        boolean[][] isPre = new boolean[numCourses][numCourses];
+        for (int[] p : prerequisites) {
+            ++indgree[p[1]];
+            g[p[0]].add(p[1]);
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; ++i) {
+            if (indgree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int ne : g[cur]) {
+                isPre[cur][ne] = true;
+                for (int i = 0; i < numCourses; ++i) {
+                    isPre[i][ne] = isPre[i][ne] | isPre[i][cur];
+                }
+                --indgree[ne];
+                if (indgree[ne] == 0) {
+                    queue.offer(ne);
+                }
+            }
+        }
+        List<Boolean> res = new ArrayList<>();
+        for (int[] query : queries) {
+            res.add(isPre[query[0]][query[1]]);
+        }
+        return res;
+    }
+
+}
---
+++ src/test/medium/CourseScheduleIVTest.java
@@ -0,0 +1,25 @@
+package test.medium;
+
+import main.medium.CourseScheduleIV;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author HJ
+ * @date 2023/9/12 09:28
+ * @description
+ */
+class CourseScheduleIVTest {
+
+    @Test
+    void checkIfPrerequisite() {
+
+        CourseScheduleIV courseScheduleIV = new CourseScheduleIV();
+        int numCourses = 4;
+        int[][] prerequisites = new int[][] {{2, 3}, {2, 1}, {0, 3}, {0, 1}};
+        int[][] queries = new int[][] {{0, 1}, {0, 3}, {2, 3}, {3, 0}, {2, 0}, {0, 2}};
+        System.out.println(courseScheduleIV.checkIfPrerequisite(numCourses, prerequisites, queries));
+
+    }
+}
