aab52a7a8b2ad56efbce2008281829f8dfd440a7
贺佳 <hejia1744709356@163.com>
2024-03-09 00:02:45 +0800

    2834. 找出美丽数组的最小和

--- src/main/medium/FindMinimumPossibleSumOfBeautifulArray.java
+++ src/main/medium/FindMinimumPossibleSumOfBeautifulArray.java
@@ -1,8 +1,8 @@
 package main.medium;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 2834. 找出美丽数组的最小和
@@ -12,18 +12,38 @@ public class FindMinimumPossibleSumOfBeautifulArray {
     public int minimumPossibleSum(int n, int target) {
         int[] beautifulArray = new int[n];
         beautifulArray[0] = 1;
-        List<Integer> list = new ArrayList<>();
-        list.add(target - 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(target - 1, target - 1);
         int j = 2;
         for (int i = 1; i < n; ) {
-            if (!list.contains(j)) {
+            if (!map.containsKey(j)) {
                 beautifulArray[i++] = j;
-                list.add(target - j);
+                if (j < target) {
+                    map.put(target - j, target - 1);
+                }
+
             }
             j++;
         }
 
         return Arrays.stream(beautifulArray).sum();
+    }
+
+    /**
+     * 官方
+     *
+     * @param n      n
+     * @param target target
+     * @return 结果
+     */
+    public int minimumPossibleSum2(int n, int target) {
+        final int MOD = (int) 1e9 + 7;
+        int m = target / 2;
+        if (n <= m) {
+            return (int) ((long) (1 + n) * n / 2 % MOD);
+        }
+        return (int) (((long) (1 + m) * m / 2 +
+                ((long) target + target + (n - m) - 1) * (n - m) / 2) % MOD);
     }
 
 }
--- src/test/medium/FindMinimumPossibleSumOfBeautifulArrayTest.java
+++ src/test/medium/FindMinimumPossibleSumOfBeautifulArrayTest.java
@@ -3,13 +3,11 @@ package test.medium;
 import main.medium.FindMinimumPossibleSumOfBeautifulArray;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FindMinimumPossibleSumOfBeautifulArrayTest {
 
     @Test
     void minimumPossibleSum() {
         FindMinimumPossibleSumOfBeautifulArray fmpsba = new FindMinimumPossibleSumOfBeautifulArray();
-        System.out.println(fmpsba.minimumPossibleSum(16, 6));
+        System.out.println(fmpsba.minimumPossibleSum(39636, 49035));
     }
 }
