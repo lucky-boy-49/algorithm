2a82c70a19d9eb3163d6dfe153588f753f26c6c7
lucky <hejia1744709356@163.com>
2024-10-11 22:49:54 +0800

    149. 直线上最多的点数

---
+++ src/main/difficulty/S149Test.java
@@ -0,0 +1,14 @@
+package main.difficulty;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class S149Test {
+
+    @Test
+    void maxPoints() {
+        S149 s149 = new S149();
+        assertEquals(6, s149.maxPoints(new int[][]{{0, 0}}));
+    }
+}
